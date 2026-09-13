5b08d38517b55823cd2ee56423c906d0e560439d
Alagusankar <alagusankar98@gmail.com>
2026-09-13 17:15:58 +0530

    Solution when array is mutable

--- 03-Arrays-Hashing/duplicate_integer.cpp
+++ 03-Arrays-Hashing/duplicate_integer.cpp
@@ -1,10 +1,8 @@
 #include <unordered_set>
 
-bool hasDuplicate(const std::vector<int>& nums) {
-    std::unordered_set<int> seenNumbers;
-    seenNumbers.reserve(nums.size()); // To reduce rehashing
-    for(const int num : nums){
-        if (auto [~, inserted] = seenNumbers.insert(num); !inserted) return true;
-    }
-    return false;
+bool hasDuplicate( std::vector<int>& nums) {
+
+    std::sort(nums.begin(), nums.end(), std::less<int>());
+
+    return std::adjacent_find(nums.begin(), nums.end()) != nums.end();
 }
