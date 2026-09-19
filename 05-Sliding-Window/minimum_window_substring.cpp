de727863bfbeff1237132333e4ec11b69d3d6dd2
Alagusankar <alagusankar98@gmail.com>
2026-09-19 14:59:32 +0530

    Optimal solution for Minimum substring

--- 05-Sliding-Window/minimum_window_substring.cpp
+++ 05-Sliding-Window/minimum_window_substring.cpp
@@ -1,39 +1,26 @@
 std::string minWindow(std::string_view s, std::string_view t) {
     if(t.size() > s.size()) return "";
 
-    std::array<int, 128> tCount{};
+    std::array<int, 128> diffCount{};
     std::array<int, 128> sCount{};
 
-    for(size_t i = 0; i < t.size(); i++){
-        int charIdx = static_cast<unsigned char>(t[i]);
-        tCount[charIdx]++;
+    for(const unsigned char c : t){
+        diffCount[c]++;
     }
 
-    int tUniqueCharacters = 0;
-    for(size_t i = 0; i < tCount.size(); i++){
-        tUniqueCharacters += (tCount[i] != 0);
-    }
-
-    int windowMatchCharacters = 0;
+    int totalCharactersRequired = std::ssize(t);
     size_t minStart = 0;
     size_t minLength = std::numeric_limits<size_t>::max();
 
     for(size_t left = 0, right = 0; right < s.size(); right++){
         int rightCharIdx = static_cast<unsigned char>(s[right]);
-        sCount[rightCharIdx]++;
+        if(diffCount[rightCharIdx] > 0) totalCharactersRequired--;  // Decrement only for characters already in diffCount
 
-        if(sCount[rightCharIdx] == tCount[rightCharIdx]) windowMatchCharacters++;
+        diffCount[rightCharIdx]--; // Ingest right character
 
-        if(windowMatchCharacters == tUniqueCharacters){
-            // Keep right as anchor
-            // Have to shrink left to see how much we can go before losing desired character
-            while(left < right){
-                int leftCharIdx = static_cast<unsigned char>(s[left]);
-                if((tCount[leftCharIdx] > 0) && sCount[leftCharIdx] == tCount[leftCharIdx]) break; // Exit if matches are about to get spoiled
-                sCount[leftCharIdx]--;
-                left++;
-            }
-
+        // Keep right as anchor
+        // Have to shrink left to see how much we can go before losing desired character
+        while(totalCharactersRequired == 0){
             // [left, right] holds a substring of t in s.
             // Check if [left, right] is less than minimum seen so far
             size_t currentMinLength = right - left + 1;
@@ -42,9 +29,9 @@ std::string minWindow(std::string_view s, std::string_view t) {
                 minLength = currentMinLength;
             }
 
-            // Move left out of valid window for next iteration
-            sCount[static_cast<unsigned char>(s[left])]--;
-            windowMatchCharacters--;
+            int leftCharIdx = static_cast<unsigned char>(s[left]);
+            if(diffCount[leftCharIdx] == 0) totalCharactersRequired++; // I'm about to move left past a needed character
+            diffCount[leftCharIdx]++;
             left++;
         }
     }
