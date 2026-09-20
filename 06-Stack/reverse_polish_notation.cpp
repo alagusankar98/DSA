c5154298a1f5657e876518c217ebf50b8e4df865
Alagusankar <alagusankar98@gmail.com>
2026-09-20 13:56:48 +0530

    Optimal solution for RPN

--- 06-Stack/reverse_polish_notation.cpp
+++ 06-Stack/reverse_polish_notation.cpp
@@ -1,42 +1,57 @@
-int parseInteger(std::string_view str){
-    int result = 0;
-
-    auto _ = std::from_chars(str.data(), str.data() + str.size(), result);
-
-    return result;
-}
 int evalRPN(const vector<string>& tokens) {
+    if(tokens.empty()) return 0;
     std::vector<int> numStack;
 
-    for(size_t i = 0; i < tokens.size(); i++){
-        if(tokens[i] == "+"){
-            int second = numStack.back();
-            numStack.pop_back();
-            int first = numStack.back();
-            numStack.pop_back();
-            numStack.push_back(first + second);
-        } else if (tokens[i] == "*") {
-            int second = numStack.back();
-            numStack.pop_back();
-            int first = numStack.back();
-            numStack.pop_back();
-            numStack.push_back(first * second);
-        } else if (tokens[i] == "/") {
-            int second = numStack.back();
-            numStack.pop_back();
-            int first = numStack.back();
-            numStack.pop_back();
-            numStack.push_back(first / second);
-        } else if (tokens[i] == "-") {
-            int second = numStack.back();
-            numStack.pop_back();
-            int first = numStack.back();
-            numStack.pop_back();
-            numStack.push_back(first - second);
+    // Each operand reduce two numbers to 1
+    // Final result has to be 1 number
+    // For 'n' numbers, there has to be 'n-1' operands
+    // Therefore, total number of elements in RPN expression is '2*n - 1'
+    // Out of '2*n - 1', only 'n' is stored in stack
+    // So 'N = (2 * n) - 1' => 'n = (N + 1) / 2' for stack reserve
+    numStack.reserve((tokens.size() + 1 )/ 2);
+
+    for(const auto& str : tokens){
+        if(str.size() == 1 && !std::isdigit(str[0])){
+            // This is an operator
+
+            // Check if stack has atleast two elements
+            assert((numStack.size() > 1) && "Stack either empty or has only one operand");
+            int second = numStack.back(); numStack.pop_back();
+            int first = numStack.back(); numStack.pop_back();
+            switch(str[0]){ // Switch works only on char (convertible to int), not on string
+            case '+': 
+                numStack.push_back(first + second); break;
+            case '-':
+                numStack.push_back(first - second); break;
+            case '*':
+                numStack.push_back(first * second); break;
+            case '/':
+                numStack.push_back(first / second); break;
+            default:
+                assert(false && "Invalid operand");
+                break;
+            }
         } else {
-            
-            numStack.push_back(parseInteger(tokens[i]));
+            // This is an operand (number)
+
+            int result = 0;
+
+            auto first = str.data();
+            const char* last = str.data() + str.size();
+
+            // Decay "+5" to "5" before invoking from_chars
+            if((first != last) && (*first == '+')){
+                first++;
+            }
+
+            auto [_, ec] = std::from_chars(first, last, result);
+
+            assert(ec == std::errc() && "Error during number parsing");
+
+            numStack.push_back(result);
         }
+        
     }
+    assert(numStack.size() == 1 && "Decayed final stack does not result in single number");
     return numStack.back();
 }
