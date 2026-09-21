7eecdb9a59ece668941f3d9a95e1268d27a1cc68
Alagusankar <alagusankar98@gmail.com>
2026-09-22 01:25:37 +0530

    Optimal solution for finding element using two passes

--- 07-Binary-Search/search_2d_matrix.cpp
+++ 07-Binary-Search/search_2d_matrix.cpp
@@ -1,45 +1,45 @@
 bool searchMatrix(const std::vector<std::vector<int>>& matrix, int target) {
-    if(matrix.empty()) return false;
+if(matrix.empty() || matrix[0].empty()) return false;
     const int rows = static_cast<int>(matrix.size());
     const int cols = static_cast<int>(matrix[0].size());
 
     int matchRow = -1;
-    int upper = 0;
-    int lower = rows - 1;
+    int top = 0;
+    int bottom = rows - 1;
 
     // Find row
-    while(upper <= lower){
-        int mid = upper + (lower - upper) / 2;
+    while(top <= bottom){
+        int mid = top + (bottom - top) / 2;
 
         if(matrix[mid][0] <= target && matrix[mid][cols - 1] >= target){
             matchRow = mid;
             break;
         } else if(matrix[mid][0] > target){
-            // Discard lower half
-            lower = mid - 1;
+            // Discard bottom half
+            bottom = mid - 1;
         } else {
-            // Discard upper half
-            upper = mid + 1;
+            // Discard top half
+            top = mid + 1;
         }
     }
 
     if(matchRow == -1) return false; // No matching row found
 
     // Find element match
-    upper = 0;
-    lower = cols - 1;
+    top = 0;
+    bottom = cols - 1;
 
-    while(upper <= lower){
-        int mid = upper + (lower - upper) / 2;
+    while(top <= bottom){
+        int mid = top + (bottom - top) / 2;
 
         if(matrix[matchRow][mid] == target){
             return true;
         } else if(matrix[matchRow][mid] > target){
-            // Discard lower half
-            lower = mid - 1;
+            // Discard bottom half
+            bottom = mid - 1;
         } else {
-            // Discard upper half
-            upper = mid + 1;
+            // Discard top half
+            top = mid + 1;
         }
     }
 
