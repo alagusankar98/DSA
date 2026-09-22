2e9881c3487fef570a300d21265a8293b5aba8ce
Alagusankar <alagusankar98@gmail.com>
2026-09-22 14:52:26 +0530

    Optimal solution for Koko eats Bananas

--- 07-Binary-Search/koko_eats_bananas.cpp
+++ 07-Binary-Search/koko_eats_bananas.cpp
@@ -1,23 +1,24 @@
 int minEatingSpeed(const std::vector<int>& piles, int h) {
+    if(piles.empty()) return 0;
     int left = 1; // Cannot eat 0 bananas
-    int right = *(std::max_element(piles.begin(), piles.end())); // Worse case for right
-    int minH = std::numeric_limits<int>::max();
+    int right = *(std::max_element(piles.begin(), piles.end())); // Worse case for right, check for emptiness to avoid nullptr
 
     while(left <= right){
         int mid = left + (right - left) / 2;
         int64_t currentHours = 0;
 
         for(const int pile : piles){
-            currentHours += (pile / mid) + ((pile % mid == 0) ? 0 : 1);
+            currentHours += ((pile + mid - 1) / mid);
         }
 
         if(currentHours <= h){
-            minH = std::min(minH, mid);
+            // Koko eats bananas fast enough, slow down eating rate
             right = mid - 1;
         } else {
+            // Koko is eating slower than target, speed up eating rate
             left = mid + 1;
         }
     }
 
-    return minH;
+    return left;
 }
